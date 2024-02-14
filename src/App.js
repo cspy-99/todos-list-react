50bfe69c72328ed61ef56279dea39e727f7489f9
Kacper Popiński <kacpi5a5@o2.pl>
2024-02-14 01:12:25 +0100

    added blocking from empty forms

--- src/App.js
+++ src/App.js
@@ -41,6 +41,17 @@ function App() {
     )
   }
 
+  const addNewTask = (content) => {
+    setTasks((tasks) => [
+      ...tasks,
+      {
+        content,
+        done: false,
+        id: tasks.length ? tasks[tasks.length - 1].id + 1 : 1,
+      },
+    ])
+  }
+
   return (
     <Container>
       <div className="form__task">
@@ -48,7 +59,10 @@ function App() {
           <h1>Lista zadań </h1>
         </header>
 
-        <Section title="Dodaj nowe zadanie" body={<Form />} />
+        <Section
+          title="Dodaj nowe zadanie"
+          body={<Form addNewTask={addNewTask} />}
+        />
         <Section
           title="Lista zadań"
           body={
--- src/Form/index.js
+++ src/Form/index.js
@@ -1,12 +1,31 @@
+import React, { useState } from 'react'
 import './style.css'
 
-const Form = () => (
-  <form className="form">
-    <div className="form__addNewTask">
-      <input className="form__entryField" placeholder="Co jest do zrobienia?" />
-      <button className="form__newTaskButton">Dodaj zadanie</button>
-    </div>
-  </form>
-)
+const Form = ({ addNewTask }) => {
+  const [newTaskContent, setNewTaskContent] = useState('')
+
+  const onFormSubmit = (event) => { 
+    if (!newTaskContent.length) {
+      newTaskContent.innerHTML = "";
+      return;}
+    event.preventDefault()
+    addNewTask(newTaskContent.trim())
+    setNewTaskContent('')
+  };
+
+  return (
+    <form className="form" onSubmit={onFormSubmit}>
+      <div className="form__addNewTask">
+        <input
+          value={newTaskContent}
+          className="form__entryField"
+          placeholder="Co jest do zrobienia?"
+          onChange={({ target }) => setNewTaskContent(target.value)}
+        />
+        <button className="form__newTaskButton">Dodaj zadanie</button>
+      </div>
+    </form>
+  )
+}
 
 export default Form
